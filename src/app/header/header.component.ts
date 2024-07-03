7c5938a0e6d698a334a95f1e81cec8ad87f668ca
Mathis <msjemenblc@gmail.com>
2024-07-03 09:30:49 +0200

    logo changed, responsive working

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,9 @@
 // Angular imports
 import { Component } from '@angular/core';
 
-// Icon imports
-import { faTrophy } from '@fortawesome/free-solid-svg-icons';
-
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-    faTrophy = faTrophy; // Icon declaration
-}
+export class HeaderComponent {}
